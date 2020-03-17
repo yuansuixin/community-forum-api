dc17d42c47059fe1e3b10ab50bae151865f45ba6
macaoyuan <macaoyuan@peiwo.cn>
2020-03-17 19:55:20 +0800

    feat: add mongoose

---
+++ src/config/index.js
@@ -0,0 +1,3 @@
+const DB_URL = "mongodb://test:123456@localhost:27017/testdb";
+
+export default { DB_URL };
---
+++ src/model/demo.js
@@ -0,0 +1,38 @@
+import User from "./test";
+
+const user = {
+  name: "brian",
+  age: 30,
+  email: "briam@imooc.com"
+};
+
+// 增
+const addMethods = async () => {
+  const data = new User(user);
+  const result = await data.save();
+  console.log(result);
+};
+addMethods();
+
+//执行命令  npx babel-node src/model/demo.js
+
+//查
+const findMethods = async () => {
+  const result = await User.find();
+  console.log(result);
+};
+findMethods();
+
+//改
+const updateMethods = async () => {
+  const result = await User.updateOne({ name: "brian" }, { age: 23 });
+  console.log(result);
+};
+updateMethods();
+
+//删
+const delMethods = async () => {
+  const result = await User.deleteOne({ name: "brian" });
+  console.log(result);
+};
+delMethods();
