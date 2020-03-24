301306e368aaa08a68512d0a9d50780e55ec3eff
macaoyuan <macaoyuan@peiwo.cn>
2020-03-24 17:55:06 +0800

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: "eslint:recommended",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "space-before-function-paren": 0,
+    "require-atomic-updates": "off"
+  }
+};
--- src/config/MailConfig.js
+++ src/config/MailConfig.js
@@ -9,14 +9,14 @@ async function send(sendInfo) {
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
+    host: 'smtp.ethereal.email',
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
       user: '2639049615@qq.com', // generated ethereal user
       pass: 'chglhjkwgvecdiee' // generated ethereal password
     }
-  });
+  })
   // let sendInfo = {
   //   code:'1324',
   //   expire:'2019-20-01',
@@ -34,30 +34,26 @@ async function send(sendInfo) {
       sendInfo.user !== ''
         ? `你好开发者，${sendInfo.user}！《慕课网前端全栈实践》注册码`
         : '《慕课网前端全栈实践》注册码', // Subject line
-    text: `您在《慕课网前端全栈实践》课程中注册，您的邀请码是${
-      sendInfo.code
-    },邀请码的过期时间: ${sendInfo.expire}`, // plain text body
+    text: `您在《慕课网前端全栈实践》课程中注册，您的邀请码是${sendInfo.code},邀请码的过期时间: ${sendInfo.expire}`, // plain text body
     html: `
         <div style="border: 1px solid #dcdcdc;color: #676767;width: 600px; margin: 0 auto; padding-bottom: 50px;position: relative;">
         <div style="height: 60px; background: #393d49; line-height: 60px; color: #58a36f; font-size: 18px;padding-left: 10px;">Imooc社区——欢迎来到官方社区</div>
         <div style="padding: 25px">
-          <div>您好，${sendInfo.user}童鞋，重置链接有效时间30分钟，请在${
-      sendInfo.expire
-    }之前重置您的密码：</div>
+          <div>您好，${sendInfo.user}童鞋，重置链接有效时间30分钟，请在${sendInfo.expire}之前重置您的密码：</div>
           <a href="${url}" style="padding: 10px 20px; color: #fff; background: #009e94; display: inline-block;margin: 15px 0;">立即重置密码</a>
           <div style="padding: 5px; background: #f2f2f2;">如果该邮件不是由你本人操作，请勿进行激活！否则你的邮箱将会被他人绑定。</div>
         </div>
         <div style="background: #fafafa; color: #b4b4b4;text-align: center; line-height: 45px; height: 45px; position: absolute; left: 0; bottom: 0;width: 100%;">系统邮件，请勿直接回复</div>
     </div>
-    `, // html body
+    ` // html body
   })
 
-
-  return "Message sent: %s", info.messageId
+  return 'Message sent: %s', info.messageId
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
   // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // eslint-disable-next-line no-unreachable
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
